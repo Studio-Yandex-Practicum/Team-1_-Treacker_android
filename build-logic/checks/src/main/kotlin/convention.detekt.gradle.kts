02f48a4d3f89da15c550ffb7899c7aa7a62be263
Guess56 <Guess56@yandex.ru>
2025-03-28 15:13:27 +0500

    исправлен путь в detekt

--- build-logic/checks/src/main/kotlin/convention.detekt.gradle.kts
+++ build-logic/checks/src/main/kotlin/convention.detekt.gradle.kts
@@ -40,7 +40,7 @@ val detektAll by tasks.register<Detekt>("detektAll") {
     setupCommonDetektSettings()
 
     // Configuration
-    config.setFrom(files(project.rootDir.resolve("conf/detekt.yml")))
+    config.setFrom(files(project.rootDir.resolve("detekt.yml")))
 }
 
 val detektFormat by tasks.register<Detekt>("detektFormat") {
@@ -50,7 +50,7 @@ val detektFormat by tasks.register<Detekt>("detektFormat") {
     autoCorrect = true
 
     // Configuration
-    config.setFrom(files(project.rootDir.resolve("conf/detekt.yml")))
+    config.setFrom(files(project.rootDir.resolve("detekt.yml")))
 }
 
 val detektProjectBaseline by tasks.register<DetektCreateBaselineTask>("detektProjectBaseline") {
@@ -74,7 +74,7 @@ val detektProjectBaseline by tasks.register<DetektCreateBaselineTask>("detektPro
     }
 
     // Configuration
-    config.setFrom(files(project.rootDir.resolve("conf/detekt.yml")))
+    config.setFrom(files(project.rootDir.resolve("detekt.yml")))
 }
 
 // workaround for https://github.com/gradle/gradle/issues/15383
