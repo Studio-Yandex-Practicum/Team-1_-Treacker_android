b1fdfe6e5f7049d08814f5e2dfd951533ebfe7bf
Guess56 <Guess56@yandex.ru>
2025-03-30 18:52:14 +0500

    пробел

--- app/src/main/java/com/example/tracker/MainActivity.kt
+++ app/src/main/java/com/example/tracker/MainActivity.kt
@@ -4,7 +4,8 @@ import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity(){
+class MainActivity : AppCompatActivity() {
+    val nAme = ""
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
