7caa2604222b646eef62e6893fed9bedec3be77a
Guess56 <Guess56@yandex.ru>
2025-04-08 12:23:16 +0500

    detekt

--- app/src/main/java/com/example/tracker/authorization/ui/AuthorizationFragment.kt
+++ app/src/main/java/com/example/tracker/authorization/ui/AuthorizationFragment.kt
@@ -178,7 +178,7 @@ class AuthorizationFragment : Fragment() {
                     if (state.message.equals("Ошибка: 401 - ")) {
                         refreshToken = sharedPreferences.getString("refresh_token", null) ?: ""
                         viewModel.refresh(refreshToken)
-                    } else if (state.message.equals("Сетевая ошибка: Failed to connect to /130.193.44.66:8080")){
+                    } else if (state.message.equals("Сетевая ошибка: Failed to connect to /130.193.44.66:8080")) {
                         Log.e("login", "вход без проверки")
                     }
                 }
