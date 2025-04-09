f21c9d6fe7cbe831c930fc7cee11e7c2ecfc5ef0
ypypyxa <axypypy@yandex.ru>
2025-04-09 14:33:58 +0300

    Исправления замечаний бота

--- app/src/main/java/com/example/tracker/settings/ui/SettingsFragment.kt
+++ app/src/main/java/com/example/tracker/settings/ui/SettingsFragment.kt
@@ -5,26 +5,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.viewModels
 import com.example.tracker.R
 
 class SettingsFragment : Fragment() {
 
-    companion object {
-        fun newInstance() = SettingsFragment()
-    }
-
-    private val viewModel: SettingsViewModel by viewModels()
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-        // Use the ViewModel
-    }
-
-    override fun onCreateView(inflater: LayoutInflater,
-                              container: ViewGroup?,
-                              savedInstanceState: Bundle?
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
     ): View {
         return inflater.inflate(R.layout.fragment_settings, container, false)
     }
